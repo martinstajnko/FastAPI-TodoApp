8049472cef5960301996ca844349b023c2d55fa9
martinstajnko <stajnko.martin93@gmail.com>
2022-09-07 21:19:51 +0200

    Changed database url

--- database_postgre.py
+++ database_postgre.py
@@ -1,10 +1,9 @@
-from venv import create
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-POSTGRESQL_DATABASE_URL = 'postgresql://martin:test123@localhost/todoappdb'
+POSTGRESQL_DATABASE_URL = 'postgresql+psycopg2://martin:test123@localhost/todoappdb'
 # As a user we need to use superuser
 
 #SQLAlchemy engine
